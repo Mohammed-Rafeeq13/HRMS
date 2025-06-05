0d3bc7b78f399b28c3327ec307a25f25f9cfd861
Kunal Nagar <kunalnagar318@gmail.com>
2025-06-05 20:16:10 +0530

    Update Loding.jsx

--- client/src/components/newsfeed/Loding.jsx
+++ client/src/components/newsfeed/Loding.jsx
@@ -37,43 +37,4 @@ const Loding = () => {
   );
 };
 
-export default Loding;import React from "react";
-
-const Loding = () => {
-  return (
-    <div className=" gap-4 p-4 max-w-[500px] m-auto bg-white shadow-md rounded-lg mt-4">
-      <div className="  gap-4 p-4 max-w-[500px] m-auto bg-white shadow-md rounded-lg mt-4">
-        <div className="flex gap-3">
-          <div>
-            <div style={{borderRadius:50+"%"}} className="rounded-[50%]  loading h-[50px] w-[50px] border-[1px] border-gray-300"></div>
-          </div>
-          <div>
-            <div className=" font-semibold loading">Dewarat singh deenanath</div>
-            <div className=" text-gray-500  loading">2 hours</div>
-          </div>
-        </div>
-        <div className="ancizar-sans-italic  loading">
-          This is a sample post description that might be longer than 80
-          characters, so it will be truncated.
-        </div>
-        <center>
-          <div className="loading h-[400px] w-[450px] "></div>
-        </center>
-        <div className="flex =mt-2   text-gray-500 mt-[10px] ">
-          <div className="action-item flex items-center  loading gap-1 mr-4">
-            <span className="material-symbols-outlined">thumb_up</span>
-            <span className="action-count">10</span>
-            <span className="action-label">Like</span>
-          </div>
-          <div className="action-item flex items-center  loading gap-1">
-            <span className="material-symbols-outlined">comment</span>
-            <span className="action-count">5</span>
-            <span className="action-label">Comment</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
 export default Loding;
